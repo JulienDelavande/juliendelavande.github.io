68205a4dd71f671c071323f27fe24355736e7298
Julien Delavande <Julien.DELAVANDE@student.isae-supaero.fr>
2024-10-04 20:17:27 +0200

    new articles new site

--- scripts.sh
+++ scripts.sh
@@ -1,7 +1,7 @@
 docker build --no-cache -t delavandejulien/site-perso:1.1 .
 docker push delavandejulien/site-perso:1.1
 
-kubectl apply -f k8s/
+#kubectl apply -f k8s/
 
 kubectl config set-context --current --namespace=site-perso
 kubectl rollout restart deployment site-perso
