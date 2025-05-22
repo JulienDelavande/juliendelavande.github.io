08a766b8ed06b04f9612fb743b73419bc31c80e2
JulienDelavande <julien.delavande@huggingface.co>
2025-05-22 16:06:32 +0200

    research section

--- scripts.sh
+++ scripts.sh
@@ -1,7 +1,9 @@
 docker build --no-cache -t juliendelavande/site-perso:1.2 .
 docker push juliendelavande/site-perso:1.2
 
-#kubectl apply -f k8s/
-
 kubectl config set-context --current --namespace=site-perso
-kubectl rollout restart deployment site-perso
+
+# if modification in k8s do
+kubectl apply -f k8s/
+
+#kubectl rollout restart deployment site-perso
