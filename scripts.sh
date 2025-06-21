13ba360b257b20001991ff904151d2666ee150c6
JulienDelavande <julien.delavande@huggingface.co>
2025-06-21 17:31:34 +0200

    new articles and prjects links

--- scripts.sh
+++ scripts.sh
@@ -1,4 +1,6 @@
 docker build --no-cache -t juliendelavande/site-perso:1.2 .
+docker buildx build --no-cache --platform linux/amd64,linux/arm64 -t juliendelavande/site-perso:1.2 . --push
+
 docker push juliendelavande/site-perso:1.2
 
 kubectl config set-context --current --namespace=site-perso
@@ -6,4 +8,4 @@ kubectl config set-context --current --namespace=site-perso
 # if modification in k8s do
 kubectl apply -f k8s/
 
-#kubectl rollout restart deployment site-perso
+kubectl rollout restart deployment site-perso
